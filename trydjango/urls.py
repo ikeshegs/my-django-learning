54b286de17bdfd22b05bf3bfe653e604a9ce84c5
Ikechukwu Okoro <ikeshegs@gmail.com>
2024-12-31 17:31:31 +0100

    basic HTML form

--- trydjango/urls.py
+++ trydjango/urls.py
@@ -23,6 +23,7 @@ from articles import views
 urlpatterns = [
     path('', home_view),   # index / root / home
     path('articles/', views.article_search_view),
+    path('articles/create/', views.article_create_view),
     path('articles/<int:id>/', views.article_detail_view),  # dynamic url routing
     path('admin/', admin.site.urls),
 ]
